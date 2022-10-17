be69ba2701cc0dcafaa47e42e632773ed74308b1
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 18:03:19 -0300

    FIX: lint problems

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -1,3 +1,10 @@
-db.produtos.updateMany({ nome: "Big Mac"}, { $unset: { curtidas: "" } });
+db.produtos.updateMany(
+  { 
+    nome: "Big Mac",
+  },
+  { 
+    $unset: { curtidas: "" } 
+  },
+);
 
 db.produtos.find({}, { _id: false, nome: true, curtidas: true });
