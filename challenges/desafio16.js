8099d923a30505d2f33d34c6ec02191b8cf4c8c7
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 16:28:14 -0300

    FEAT: Query to set tag ultimaModificacao to Big Mac

---
+++ challenges/desafio16.js
@@ -0,0 +1,18 @@
+db.produtos.updateOne({
+  nome: "Big Mac",
+}, {
+  $currentDate: {
+    ultimaModificacao: {
+      $type: "date",
+    },
+  },
+});
+
+db.produtos.find({
+  ultimaModificacao: {
+    $exists: true,
+  },
+}, {
+  _id: false,
+  nome: true,
+});
