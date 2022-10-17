1c6f0caf6436229e807e9f36aace7515cdd10c01
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 15:55:03 -0300

    FEAT: Query to add Ketchup to all products but McChicken

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({
+    nome: { $ne: "McChicken" }
+  },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
+
+
+db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
