8bbe1bbd99e62a3c9ba0750c717a0fb2fb83ccf6
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 16:46:30 -0300

    FEAT: Query to add BACON to Big Mac and Quarteirao com Queijo ingredients

---
+++ challenges/desafio18.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany({
+  nome: {
+    $in: ["Big Mac", "Quarteirão com Queijo"],
+  }
+}, {
+  $addToSet: {
+    ingredientes: "bacon",
+  },
+});
+
+db.produtos.find({}, {
+  _id: 0,
+  nome: 1,
+  ingredientes: 1,
+});
