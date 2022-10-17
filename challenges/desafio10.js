985cef61ed4a5a0b152d4272102ef4f11bd27247
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 15:41:30 -0300

    FEAT: Query to search products who has a protein percentual >= 30 and <= 40

---
+++ challenges/desafio10.js
@@ -0,0 +1,14 @@
+db.produtos.find({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "proteínas",
+      percentual: {
+        $gte: 30,
+        $lte: 40
+      }
+    }
+  }
+}, {
+  _id: false,
+  nome: true
+});
