933ea33e03ce63e602fef37e934bbc640d7a4bdf
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 16:41:04 -0300

    FEAT: Query to insert franquia, and totalProdutos in resumoProdutos

---
+++ challenges/desafio17.js
@@ -0,0 +1,10 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.find().count(),
+});
+
+db.resumoProdutos.find({}, {
+  _id: false,
+  franquia: true,
+  totalProdutos: true,
+});
