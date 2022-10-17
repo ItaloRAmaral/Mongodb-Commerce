5177f4bd9f5106a39d5b93bd8094f6e7c80c5cee
Italo Amaral <italorockamaral@gmail.com>
2022-10-17 15:07:53 -0300

    FEAT: Query to search product who has only 36 likes or 85 sales

---
+++ challenges/desafio5.js
@@ -0,0 +1,16 @@
+db.produtos.find({
+  $or: [{
+    curtidas: {
+      $eq: 36
+    }
+  }, {
+    vendidos: {
+      $eq: 85
+    }
+  }]
+}, {
+  nome: true,
+  vendidos: true,
+  curtidas: true,
+  _id: false
+});
